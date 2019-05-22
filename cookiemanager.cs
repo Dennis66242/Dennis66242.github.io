18ad0ef476aa5f3321f97f08a1a1ccad22d32ac6
Dennis66242 <47192555+Dennis66242@users.noreply.github.com>
2019-05-22 04:03:18 -0400

    Add files via upload

---
+++ cookiemanager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cookiemanager : MonoBehaviour {
+    private int cookies = 0;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
